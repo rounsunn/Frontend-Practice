fa3683191a83cbd6cd181d095adc419195a1b7b4
Sunny Kumar <rounsunn@gmail.com>
2023-07-14 22:30:00 +0530

    Added basic style to elemnts using bootstrap style - container, form, button

--- React/first_vite_react_app/LoginForm/src/component/LoginForm.tsx
+++ React/first_vite_react_app/LoginForm/src/component/LoginForm.tsx
@@ -2,12 +2,22 @@ import React from "react";
 
 const LoginForm: React.FC = () => {
   return (
-    <>
-      <h1>SignIn</h1>
-      <input type="text" placeholder="Type userName" />
-      <input type="text" placeholder="password" />
-      <button className="btn btn-primary">Submit</button>
-    </>
+    <div className="container">
+      <div className="row ">
+        <div className="col-md-6 offset-md-3">
+          <h1>SignIn</h1>
+          <input type="text" className="form-control" placeholder="username" />
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+          />
+          <button type="submit" className="btn btn-primary mt-3">
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
