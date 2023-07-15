1ebce2923f27da3533272287c8d714ef996b9db8
Sunny Kumar <rounsunn@gmail.com>
2023-07-15 13:14:01 +0530

    Validating login form using backend Data using axios.post url, email: email, password: password,;

--- React/first_vite_react_app/LoginForm/src/App.tsx
+++ React/first_vite_react_app/LoginForm/src/App.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginForm from "./component/LoginForm";
+import JobListing from "./component/JobListing";
 import "./App.css";
 
 const App: React.FC = () => {
-  return <LoginForm />;
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  // const [employeeId, setEmployeeid] = useState("");
+
+  return (
+    <div className="form-container">
+      <h1>SignIn</h1>
+      {isSubmitted ? (
+        <JobListing />
+      ) : (
+        <LoginForm setIsSubmitted={setIsSubmitted} />
+      )}
+    </div>
+  );
 };
 
 export default App;
--- React/first_vite_react_app/LoginForm/src/component/LoginForm.tsx
+++ React/first_vite_react_app/LoginForm/src/component/LoginForm.tsx
@@ -1,18 +1,96 @@
 import React, { useState } from "react";
-import Form from "./Form";
+import axios from "axios";
 
-const LoginForm: React.FC = () => {
-  const [isSubmitted, setIsSubmitted] = useState(false);
+interface FormProps {
+  setIsSubmitted: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LoginForm: React.FC<FormProps> = ({ setIsSubmitted }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessages, setErrorMessages] = useState<{
+    name: string;
+    message: string;
+  }>({
+    name: "",
+    message: "",
+  });
+
+  // const errors = {
+  //   email: "Invalid email",
+  //   pass: "Invalid password",
+  // };
+
+  const renderErrorMessage = (name: string) => {
+    return name === errorMessages.name && <p>{errorMessages.message}</p>;
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "https://dev123.gigin.ai/abc/index.php/Api_controller/login_email",
+        {
+          email: email,
+          password: password,
+        }
+      );
+      console.log("resposne: ", response);
+      if (response.data.status !== "fail") {
+        setIsSubmitted(true);
+      } else {
+        console.log(response.data);
+        setErrorMessages({
+          name: "pass",
+          message: response.data.dataset || response.data.message,
+        });
+      }
+    } catch (err) {
+      console.log("Error: ", err);
+    }
+
+    // const userData = database.find((user) => user.username === userName);
+
+    // if (userData) {
+    //   if (userData.password !== password) {
+    //     setErrorMessages({ name: "pass", message: errors.pass });
+    //   } else {
+    //     setIsSubmitted(true);
+    //   }
+    // } else {
+    //   setErrorMessages({ name: "uname", message: errors.uname });
+    // }
+  };
 
   return (
-    <div className="form-container">
-      <h1>SignIn</h1>
-      {isSubmitted ? (
-        <h3>SignIn successful</h3>
-      ) : (
-        <Form setIsSubmitted={setIsSubmitted} />
-      )}
-    </div>
+    <form onSubmit={handleSubmit} className="d-flex flex-column">
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {renderErrorMessage("email")}
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {renderErrorMessage("pass")}
+      </div>
+      <button type="submit" className="btn btn-primary mt-3">
+        Submit
+      </button>
+    </form>
   );
 };
 
